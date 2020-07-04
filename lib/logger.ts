b2faecc8802c3d43b2911c0ee6513c24f1084054
Jason Kleinberg <ustice@gmail.com>
2020-07-04 19:30:26 -0400

    Converts project to Typescript (#1)

---
+++ lib/logger.ts
@@ -0,0 +1,6 @@
+export const logger = {
+    debug: console.debug.bind(console),
+    info: console.info.bind(console),
+    warn: console.warn.bind(console),
+    error: console.error.bind(console),
+}
