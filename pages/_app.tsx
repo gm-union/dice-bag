808709833566d3662669e24edc045686c543bbd2
Graham Peel <52424652+GrahamPeel@users.noreply.github.com>
2020-07-08 10:08:08 -0400

    Basic app styles added (#3)
    * Basic app styles added
    
    * Switch to SCSS, remove cruft code
    
    Co-authored-by: Graham Peel <gpeel@Mac-mini.local>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ApolloProvider } from '@apollo/react-hooks'
 import { useApollo } from '../apollo/client'
+import './style.css';
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={apolloClient}>
+      <h1>
+        DiceBag
+      </h1>
       <Component {...pageProps} />
     </ApolloProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,14 +33,56 @@ const Index = () => {
   if (viewer) {
     return (
       <div>
-        You're signed in as {viewer.email} goto{' '}
-        <Link href="/about">
-          <a>about</a>
-        </Link>{' '}
-        page. or{' '}
-        <Link href="/signout">
-          <a>signout</a>
-        </Link>
+        <div>
+          You're signed in as {viewer.email} goto{' '}
+          <Link href="/about">
+            <a>about</a>
+          </Link>{' '}
+              page. or{' '}
+          <Link href="/signout">
+            <a>signout</a>
+          </Link>
+        </div>
+        <div class="npcApp">
+          <div>
+            <h2>My saved NPCs</h2>
+            <div class="npcListOutput">
+              <p>You have no saved NPCs yet</p>
+              <a hre="#">&#36;&#123;npc.name	&#125;</a>
+            </div>
+            <div class="randomBtnHolder">
+              <button>
+                Roll a random NPC
+              </button>
+            </div>
+          </div>
+          <div>
+            <div id="npcCardTarget">
+
+              <div class="npcCard">
+                <em>(Random Name)</em><br />
+                <span>(Random Body Type) (Random Background) (Random Race) (Random Gender)</span><br />
+                <span>Influenced by: (Random Influenced By)</span>
+              </div>
+
+            </div>
+          </div>
+          <div>
+            <div class="editGrid">
+              <input type="hidden" id="selectedNpcGuid" />
+              <label><b>Name</b><input type="text" id="editNameField" /></label>
+              <label><b>Body Type</b><input type="text" id="editBodyTypeField" /></label>
+              <label><b>Background</b><input type="text" id="editBackgroundField" /></label>
+              <label><b>Race</b><input type="text" id="editRaceField" /></label>
+              <label><b>Gender</b><input type="text" id="editGenderField" /></label>
+              <label><b>Influenced By</b><input type="text" id="editInfluencedByField" /></label>
+              <div>
+                <span></span>
+                <div><button>Save</button></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
